c5583fcc0fb429cd12a88e6e5b587dce4b2fef1c
Fra1020 <francescocoppola.lavoro@gmail.com>
2022-04-13 15:09:10 +0200

    esercizio-4

---
+++ m1/w3/d3/esercizio/js/script.js
@@ -0,0 +1,17 @@
+function calcolaAnni(compleanno) {
+    return 2022 - compleanno;
+}
+
+alert(calcolaAnni(2001));
+
+const anni = calcolaAnni(1999);
+console.log(anni);
+
+const calcolaAnni2 = compleanno => 2022 - compleanno;
+
+let anni2 = calcolaAnni2(1981);
+console.log(anni2);
+
+
+
+
